a99f4d1f1a83a6ee62fe52c1f0aa783f9ba7601b
Peter Heywood <p.heywood@sheffield.ac.uk>
2020-03-06 08:45:52 +0000

    Early boids implementation, Incomplete and untested
    No initial states file, but linted.

---
+++ examples/boids_bruteforce/src/main.cu
@@ -0,0 +1,365 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+
+
+#include "flamegpu/flame_api.h"
+#include "flamegpu/runtime/flamegpu_api.h"
+#include "flamegpu/io/factory.h"
+#include "flamegpu/util/nvtx.h"
+
+
+// Environment Bounds
+#define MIN_POSITION -0.5f
+#define MAX_POSITION +0.5f
+
+// Interaction radius
+#define INTERACTION_RADIUS 0.1f
+#define SEPARATION_RADIUS 0.005f
+
+// Global Scalers
+#define TIME_SCALE 0.0005f
+#define GLOBAL_SCALE 0.15f
+
+
+// Rule scalers
+#define STEER_SCALE 0.65f
+#define COLLISION_SCALE 0.75f
+#define MATCH_SCALE 1.25f
+
+// @todo - need to abstract __device__ away from the modeller.
+// Get the length of a vector
+inline __device__ float length(const float x, const float y, const float z) {
+    return sqrtf(x * x + y * y + z * z);
+}
+
+// Bound the agent to the environment
+// @todo - this is actually wrapping?
+__device__ void boundPosition(float &x, float &y, float &z) {
+    // @todo switch to env vars.
+    // Bound x.
+    x = (x < MIN_POSITION)? MAX_POSITION: x;
+    x = (x > MAX_POSITION)? MIN_POSITION: x;
+
+    y = (y < MIN_POSITION)? MAX_POSITION: y;
+    y = (y > MAX_POSITION)? MIN_POSITION: y;
+
+    z = (z < MIN_POSITION)? MAX_POSITION: z;
+    z = (z > MAX_POSITION)? MIN_POSITION: z;
+}
+
+
+// Agent function to output the message
+FLAMEGPU_AGENT_FUNCTION(outputdata, MsgNone, MsgBruteForce) {
+    // Output each agents publicly visible properties.
+    FLAMEGPU->message_out.setVariable<int>("id", FLAMEGPU->getVariable<int>("id"));
+    FLAMEGPU->message_out.setVariable<float>("x", FLAMEGPU->getVariable<float>("x"));
+    FLAMEGPU->message_out.setVariable<float>("y", FLAMEGPU->getVariable<float>("y"));
+    FLAMEGPU->message_out.setVariable<float>("z", FLAMEGPU->getVariable<float>("z"));
+    FLAMEGPU->message_out.setVariable<float>("fx", FLAMEGPU->getVariable<float>("fx"));
+    FLAMEGPU->message_out.setVariable<float>("fy", FLAMEGPU->getVariable<float>("fy"));
+    FLAMEGPU->message_out.setVariable<float>("fz", FLAMEGPU->getVariable<float>("fz"));
+    return ALIVE;
+}
+// Agent function to iterate messages and perform the boids behaviours.
+FLAMEGPU_AGENT_FUNCTION(inputdata, MsgBruteForce, MsgNone) {
+    // Agent properties in local register
+    int id = FLAMEGPU->getVariable<int>("id");
+    // Agent position
+    float agent_x = FLAMEGPU->getVariable<float>("x");
+    float agent_y = FLAMEGPU->getVariable<float>("y");
+    float agent_z = FLAMEGPU->getVariable<float>("z");
+    // Agent velocity
+    float agent_fx = FLAMEGPU->getVariable<float>("fx");
+    float agent_fy = FLAMEGPU->getVariable<float>("fy");
+    float agent_fz = FLAMEGPU->getVariable<float>("fz");
+
+    // Boids percieved center
+    float perceived_centre_x = 0.0f;
+    float perceived_centre_y = 0.0f;
+    float perceived_centre_z = 0.0f;
+    int perceived_count = 0;
+
+    // Boids global velocity matching
+    float global_velocity_x = 0.0f;
+    float global_velocity_y = 0.0f;
+    float global_velocity_z = 0.0f;
+
+    // Boids short range avoidance centre
+    float collision_centre_x = 0.0f;
+    float collision_centre_y = 0.0f;
+    float collision_centre_z = 0.0f;
+    int collision_count = 0;
+
+    // Iterate location messages, accumulating relevant data and counts.
+    for (const auto &message : FLAMEGPU->message_in) {
+        // Ignore self messages.
+        if (message.getVariable<int>("id") != id) {
+            // Get the message location and velocity.
+            const float message_x = message.getVariable<float>("x");
+            const float message_y = message.getVariable<float>("y");
+            const float message_z = message.getVariable<float>("z");
+            const float message_fx = message.getVariable<float>("fx");
+            const float message_fy = message.getVariable<float>("fy");
+            const float message_fz = message.getVariable<float>("fz");
+
+            // Check interaction radius
+            float separation = length(agent_x - message_x, agent_y - message_y, agent_z - message_z);
+
+            if (separation < (INTERACTION_RADIUS)) {
+                // Update the percieved centre
+                perceived_centre_x += message_x;
+                perceived_centre_y += message_y;
+                perceived_centre_z += message_z;
+                perceived_count++;
+
+                // Update percieved velocity matching
+                global_velocity_x += message_fx;
+                global_velocity_y += message_fy;
+                global_velocity_z += message_fz;
+
+                // Update collision centre
+                if (separation < (SEPARATION_RADIUS)) {  // dependant on model size
+                    collision_centre_x += message_x;
+                    collision_centre_y += message_y;
+                    collision_centre_z += message_z;
+                    collision_count += 1;
+                }
+            }
+        }
+    }
+
+    // Divide positions/velocities by relevant counts.
+    perceived_centre_x /= perceived_count;
+    perceived_centre_y /= perceived_count;
+    perceived_centre_z /= perceived_count;
+
+    global_velocity_x /= perceived_count;
+    global_velocity_y /= perceived_count;
+    global_velocity_z /= perceived_count;
+
+    collision_centre_x /= collision_count;
+    collision_centre_y /= collision_count;
+    collision_centre_z /= collision_count;
+
+
+    // Total change in velocity
+    float velocity_change_x = 0.f;
+    float velocity_change_y = 0.f;
+    float velocity_change_z = 0.f;
+
+    // Rule 1) Steer towards perceived centre of flock (Cohesion)
+    float steer_velocity_x = 0.f;
+    float steer_velocity_y = 0.f;
+    float steer_velocity_z = 0.f;
+    if (perceived_count > 0) {
+        steer_velocity_x = (perceived_centre_x - agent_x) * STEER_SCALE;
+        steer_velocity_y = (perceived_centre_y - agent_y) * STEER_SCALE;
+        steer_velocity_z = (perceived_centre_z - agent_z) * STEER_SCALE;
+    }
+    velocity_change_x += steer_velocity_x;
+    velocity_change_y += steer_velocity_y;
+    velocity_change_z += steer_velocity_z;
+
+    // Rule 2) Match neighbours speeds (Alignment)
+    float match_velocity_x = 0.f;
+    float match_velocity_y = 0.f;
+    float match_velocity_z = 0.f;
+    if (collision_count > 0) {
+        match_velocity_x = global_velocity_x * MATCH_SCALE;
+        match_velocity_y = global_velocity_y * MATCH_SCALE;
+        match_velocity_z = global_velocity_z * MATCH_SCALE;
+    }
+    velocity_change_x += match_velocity_x;
+    velocity_change_y += match_velocity_y;
+    velocity_change_z += match_velocity_z;
+
+    // Rule 3) Avoid close range neighbours (Separation)
+    float avoid_velocity_x = 0.0f;
+    float avoid_velocity_y = 0.0f;
+    float avoid_velocity_z = 0.0f;
+    if (collision_count > 0) {
+        avoid_velocity_x = (agent_x - collision_centre_x) * COLLISION_SCALE;
+        avoid_velocity_y = (agent_y - collision_centre_y) * COLLISION_SCALE;
+        avoid_velocity_z = (agent_z - collision_centre_z) * COLLISION_SCALE;
+    }
+    velocity_change_x += avoid_velocity_x;
+    velocity_change_y += avoid_velocity_y;
+    velocity_change_z += avoid_velocity_z;
+
+
+    // Global scale of velocity change
+    velocity_change_x *= GLOBAL_SCALE;
+    velocity_change_y *= GLOBAL_SCALE;
+    velocity_change_z *= GLOBAL_SCALE;
+
+    // Update agent velocity
+    agent_fx += velocity_change_x;
+    agent_fy += velocity_change_y;
+    agent_fz += velocity_change_z;
+
+    // Bound velocity
+    float agent_fscale = length(agent_fx, agent_fy, agent_fz);
+    if (agent_fscale > 1) {
+        agent_fx /= agent_fscale;
+        agent_fy /= agent_fscale;
+        agent_fz /= agent_fscale;
+    }
+
+    // Apply the velocity
+    agent_x += agent_fx * TIME_SCALE;
+    agent_y += agent_fy * TIME_SCALE;
+    agent_z += agent_fz * TIME_SCALE;
+
+    // Bound position
+    boundPosition(agent_x, agent_y, agent_z);
+
+    if (id == 0) {
+        printf("agent %d (%+1.5f, %+1.5f, %+1.5f) (%+1.5f, %+1.5f, %+1.5f)\n", id, agent_x, agent_y, agent_z, agent_fx, agent_fy, agent_fz);
+    }
+
+    // Update global agent memory.
+    FLAMEGPU->setVariable<float>("x", agent_x);
+    FLAMEGPU->setVariable<float>("y", agent_y);
+    FLAMEGPU->setVariable<float>("z", agent_z);
+
+    FLAMEGPU->setVariable<float>("fx", agent_fx);
+    FLAMEGPU->setVariable<float>("fy", agent_fy);
+    FLAMEGPU->setVariable<float>("fz", agent_fz);
+
+    return ALIVE;
+}
+
+int main(int argc, const char ** argv) {
+    ModelDescription model("Boids_BruteForce");
+
+    {   // Location message
+        MsgBruteForce::Description &message = model.newMessage("location");
+        // A message to hold the location of an agent.
+        message.newVariable<int>("id");
+        message.newVariable<float>("x");
+        message.newVariable<float>("y");
+        message.newVariable<float>("z");
+        message.newVariable<float>("fx");
+        message.newVariable<float>("fy");
+        message.newVariable<float>("fz");
+    }
+    {   // Boid agent
+        AgentDescription &agent = model.newAgent("Boid");
+        agent.newVariable<int>("id");
+        agent.newVariable<float>("x");
+        agent.newVariable<float>("y");
+        agent.newVariable<float>("z");
+        agent.newVariable<float>("fx");
+        agent.newVariable<float>("fy");
+        agent.newVariable<float>("fz");
+        agent.newFunction("outputdata", outputdata).setMessageOutput("location");
+        agent.newFunction("inputdata", inputdata).setMessageInput("location");
+    }
+
+
+    /**
+     * GLOBALS
+     */
+    {
+        EnvironmentDescription &env = model.Environment();
+
+        // Environment Bounds
+        /* env.add("MIN_POSITION", -0.5f);
+        env.add("MAX_POSITION", +0.5f);
+
+        // Interaction radius
+        env.add("INTERACTION_RADIUS", 0.1f);
+        env.add("SEPARATION_RADIUS", 0.005f);
+
+        // Global Scalers
+        env.add("TIME_SCALE", 0.0005f);
+        env.add("GLOBAL_SCALE", 0.15f);
+
+
+        // Rule scalers
+        env.add("STEER_SCALE", 0.65f);
+        env.add("COLLISION_SCALE", 0.75f);
+        env.add("MATCH_SCALE", 1.25f); */
+    }
+
+    /**
+     * Control flow
+     */     
+    {   // Attach init/step/exit functions and exit condition
+        // model.addStepFunction(Validation);
+    }
+
+    {   // Layer #1
+        LayerDescription &layer = model.newLayer();
+        layer.addAgentFunction(outputdata);
+    }
+    {   // Layer #2
+        LayerDescription &layer = model.newLayer();
+        layer.addAgentFunction(inputdata);
+    }
+
+
+    /**
+     * Create Model Runner
+     */
+    CUDAAgentModel cuda_model(model);
+
+    /**
+     * Initialisation
+     */
+    cuda_model.initialise(argc, argv);
+
+    // If no xml model file was is provided, generate a population.
+    if (cuda_model.getSimulationConfig().xml_input_file.empty()) {
+        // Currently population has not been init, so generate an agent population on the fly
+        const unsigned int AGENT_COUNT = 32;
+        // @todo better RNG / seeding. Multiple distributions from multiple seeds (generated from a single, cli-based seed)
+        std::default_random_engine rng;
+        std::uniform_real_distribution<float> pos_dist(MIN_POSITION, MAX_POSITION);
+        std::uniform_real_distribution<float> velocity_dist(-1, 1);
+        AgentPopulation population(model.Agent("Boid"), AGENT_COUNT);
+        for (unsigned int i = 0; i < AGENT_COUNT; i++) {
+            AgentInstance instance = population.getNextInstance();
+            instance.setVariable<int>("id", i);
+            instance.setVariable<float>("x", pos_dist(rng));
+            instance.setVariable<float>("y", pos_dist(rng));
+            instance.setVariable<float>("z", pos_dist(rng));
+            // @todo - normalise the velocity?
+            instance.setVariable<float>("fx", velocity_dist(rng));
+            instance.setVariable<float>("fy", velocity_dist(rng));
+            instance.setVariable<float>("fz", velocity_dist(rng));
+            printf(
+                "new boid %d at (%+1.5f, %+1.5f, %+1.5f), with velocity (%+1.5f, %+1.5f, %+1.5f)\n",
+                instance.getVariable<int>("id"),
+                instance.getVariable<float>("x"),
+                instance.getVariable<float>("y"),
+                instance.getVariable<float>("z"),
+                instance.getVariable<float>("fx"),
+                instance.getVariable<float>("fy"),
+                instance.getVariable<float>("fz"));
+        }
+        cuda_model.setPopulationData(population);
+    }
+
+    /**
+     * Execution
+     */
+    cuda_model.simulate();
+
+
+    /**
+     * Export Pop
+     */
+    // cuda_model.output();
+    // Based on Simulation::output() // That can't currently be called
+    // std::unordered_map<std::string, std::shared_ptr<AgentPopulation>> pops;
+    // auto a = std::make_shared<AgentPopulation>(model.getAgent("Boid"));
+    // cuda_model.getPopulationData(*a);
+    // pops.emplace("Boid", a);
+    // StateWriter *write__ = WriterFactory::createWriter(pops, cuda_model.getStepCounter(), "end.xml");
+    // write__->writeStates();
+    return 0;
+}
+
